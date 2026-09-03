cf79a7df355ae2a76d73eae6eedab1264d3c153e
Moses Ayite <ayitemoses@gmail.com>
2026-09-02 22:48:46 -0400

    feat: add employee controller

---
+++ ems-backend/src/main/java/com/akadev/ems/controller/EmployerController.java
@@ -0,0 +1,23 @@
+package com.akadev.ems.controller;
+
+import com.akadev.ems.dto.EmployeeDto;
+import com.akadev.ems.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployerController {
+
+    private EmployeeService employeeService;
+
+
+    // Build Add Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
