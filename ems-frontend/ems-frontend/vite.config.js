7a32007a25013744a6875e8b48be3fd987b4d08b
Moses Ayite <ayitemoses@gmail.com>
2026-09-08 21:52:38 -0400

    chore: set the server port to 3000

--- ems-frontend/ems-frontend/vite.config.js
+++ ems-frontend/ems-frontend/vite.config.js
@@ -4,4 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+
+  // Changing the server port to 3000 to avoid conflicts with other services
+  server: {
+    port: 3000
+  }
 })
